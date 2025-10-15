256edfdf5b3512331e92cf567c76f7285c214fc3
Dinh Linh <dinhlinh.work24@gmail.com>
2025-10-16 02:17:57 +0700

    init project & add header

---
+++ src/components/custom/Navigation.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "../ui/navigation-menu";
+
+export const Navigation = () => {
+    const navs: { title: string, href: string }[] = [
+        {
+            title: "Gift",
+            href: "/gift",
+        },
+        {
+            title: "How it Works",
+            href: "/how-it-works",
+        },
+        {
+            title: "About us",
+            href: "/about-us",
+        },
+        {
+            title: "Benefits",
+            href: "/benefits",
+        },
+        {
+            title: "Featured In",
+            href: "/featured-in",
+        }
+    ];
+
+    return (
+        <NavigationMenu viewport={false}>
+            <NavigationMenuList>
+                {navs.map((nav) => (
+                    <NavigationMenuItem key={nav.title} className="relative">
+                        <NavigationMenuLink asChild className={`${navigationMenuTriggerStyle()} relative after:absolute after:w-full after:h-[2px] after:-bottom-2 after:bg-secondary after:rounded-full after:scale-0 hover:after:scale-100 after:transition-transform`}>
+                            <Link href={nav.href}>{nav.title}</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                ))}
+            </NavigationMenuList>
+        </NavigationMenu>
+    )
+}
---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,26 @@
+import { Navigation } from "../custom/Navigation"
+import CartIcon from "@/assets/icons/cart.svg"
+import UserIcon from "@/assets/icons/user.svg"
+import { Badge } from "@/components/ui/badge"
+
+export const Header = () => {
+    return (
+        <header className="h-[68px] md:h-[120px] bg-primary text-primary-foreground relative flex items-center justify-center">
+            <img
+                src="./images/all-assets.png"
+                alt="Logo"
+                className="w-[166px] h-[56px] object-cover object-[0px_-10px] absolute top-8 left-[104px]"
+            />
+            <Navigation />
+            <div className="absolute top-[41px] right-[104px] flex gap-4">
+                <div className="relative">
+                    <CartIcon />
+                    <Badge className="h-[18px] w-[18px] rounded-full px-1 tabular-nums bg-secondary text-black absolute -top-1 -right-1">
+                        8
+                    </Badge>
+                </div>
+                <UserIcon />
+            </div>
+        </header>
+    )
+}
